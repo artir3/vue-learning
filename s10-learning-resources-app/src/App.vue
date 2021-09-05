29e57ea597d42d30cb3f7b5f01e6641c318e0638
Artur Markowski <markowski.artur@outlook.com>
2021-09-05 17:21:45 +0200

    init solution

---
+++ s10-learning-resources-app/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <Resources :resources="storeResources"></Resources>
+</template>
+
+<script>
+import Resources from './components/lerning-resources/Resources.vue';
+
+export default {
+  components: {
+    Resources
+  },
+  data() {
+    return {
+      storeResources: [
+        {
+          id: '1',
+          title: 'Test Title no 1',
+          description: 'This is descrption to test object 1',
+          linl: 'https://vuejs.org'
+        },
+        {
+          id: '2',
+          title: 'Test Title no 2',
+          description: 'This is descrption to test object 2',
+          linl: 'https://google.com'
+        }
+      ]
+    };
+  }
+};
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+body {
+  margin: 0%;
+}
+</style>
---
+++ s10-learning-resources-app/src/components/lerning-resources/Resources.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <resource
+      v-for="res in resources"
+      :key="res.id"
+      :link="res.link"
+      :description="res.description"
+      :title="res.title"
+    ></resource>
+  </ul>
+</template>
+
+<script>
+import Resource from './Resource.vue';
+export default {
+  components: { Resource },
+  props: ['resources']
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
