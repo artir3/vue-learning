1d0776ef33996619d2fc29be4ce1557858e83468
Artur Markowski <markowski.artur@outlook.com>
2021-09-23 21:04:54 +0200

    added dummy content, base UI elements and base storage

---
+++ vuejs-s16-find-coach-web-app/src/pages/coaches/CoachesList.vue
@@ -0,0 +1,54 @@
+<template>
+  <section>
+    <filter>Fileter list</filter>
+  </section>
+
+  <section>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register Couach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :areas="coach.areas"
+          :rate="coach.hourlyRate"
+        />
+      </ul>
+      <h3 v-else>No coaches found</h3>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+export default {
+  components: { CoachItem },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/getCoaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
---
+++ vuejs-s16-find-coach-web-app/src/store/modules/coaches/index.js
@@ -0,0 +1,32 @@
+import mutations from './mutations.js';
+import actions from './actions.js';
+import getters from './getters.js';
+
+export default {
+  namespaced: true,
+  state() {
+    return {
+      coaches: [
+        {
+          id: '123213',
+          firstName: 'Bla',
+          lastName: 'Bula',
+          areas: ['frontend', 'backend', 'career'],
+          description: 'Dummy description with dummy life',
+          hourlyRate: 30
+        },
+        {
+          id: '4324',
+          firstName: 'Donat',
+          lastName: 'Bonat',
+          areas: ['frontend', 'career'],
+          description: 'Dummy description with dummy company and aad life',
+          hourlyRate: 30
+        }
+      ]
+    };
+  },
+  mutations,
+  getters,
+  actions
+};
