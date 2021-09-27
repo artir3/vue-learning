71ea02aa9393ac8a4ecccfb22fe19e4e455e5af0
Artur Markowski <markowski.artur@outlook.com>
2021-09-27 19:26:00 +0200

    added own reactive validation to CoachForm

--- vuejs-s16-find-coach-web-app/src/components/coaches/CoachForm.vue
+++ vuejs-s16-find-coach-web-app/src/components/coaches/CoachForm.vue
@@ -1,25 +1,33 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
-      <label for="fistname">Firstname</label>
+    <div class="form-control" :class="{ invalid: !isTextValid('firstName') }">
+      <label for="fistname">
+        Firstname
+      </label>
       <input
         type="text"
         name="firstname"
         id="firstname"
         v-model.trim="firstName"
       />
+      <p v-if="!isTextValid('firstName')">Firstname must not be empty</p>
     </div>
-    <div class="form-control">
-      <label for="lastname">Lastname</label>
+    <div class="form-control" :class="{ invalid: !isTextValid('lastName') }">
+      <label for="lastname">
+        Lastname
+      </label>
       <input
         type="text"
         name="lastname"
         id="lastname"
         v-model.trim="lastName"
       />
+      <p v-if="!isTextValid('lastName')">Lastname must not be empty</p>
     </div>
-    <div class="form-control">
-      <label for="description">Description</label>
+    <div class="form-control" :class="{ invalid: !isTextValid('description') }">
+      <label for="description">
+        Description
+      </label>
       <textarea
         name="description"
         id="description"
@@ -27,13 +35,17 @@
         rows="5"
         v-model.trim="description"
       ></textarea>
+      <p v-if="!isTextValid('description')">Description must not be empty</p>
     </div>
-    <div class="form-control">
-      <label for="rate">Hourly Rate</label>
+    <div class="form-control" :class="{ invalid: !isNumberValid('rate') }">
+      <label for="rate">
+        Hourly Rate
+      </label>
       <input type="number" name="rate" id="rate" v-model.number="rate" />
+      <p v-if="!isNumberValid('rate')">Rate must be grater than zero</p>
     </div>
-    <div class="form-control">
-      <h3>Areas of expertise</h3>
+    <div class="form-control" :class="{ invalid: !isCheckboxValid('areas') }">
+      <label>Areas of expertise</label>
       <div>
         <input type="checkbox" id="frontend" value="frontend" v-model="areas" />
         <label for="frontend">Frontend Development</label>
@@ -46,7 +58,13 @@
         <input type="checkbox" id="career" value="career" v-model="areas" />
         <label for="career">Career</label>
       </div>
+      <p v-if="!isCheckboxValid('areas')">
+        At least one expertise must be selected
+      </p>
     </div>
+    <p v-if="!isValid()" class="invalid">
+      Please fix the above errors before submit
+    </p>
     <base-button>Register</base-button>
   </form>
 </template>
@@ -60,11 +78,36 @@ export default {
       lastName: '',
       description: '',
       rate: null,
-      areas: []
+      areas: [],
+      init: true
     };
   },
   methods: {
+    isTextValid(input) {
+      return this.init | (this[input] !== '');
+    },
+    isNumberValid(input) {
+      return this.init | (this[input] & (this[input] > 0));
+    },
+    isCheckboxValid(input) {
+      return this.init | (this[input].length > 0);
+    },
+    isValid() {
+      return (
+        this.isTextValid('firstName') &
+        this.isTextValid('lastName') &
+        this.isTextValid('description') &
+        this.isNumberValid('rate') &
+        this.isCheckboxValid('areas')
+      );
+    },
     submitForm() {
+      this.init = false;
+
+      if (!this.isValid()) {
+        return;
+      }
+
       const formData = {
         firstName: this.firstName,
         lastName: this.lastName,
@@ -103,6 +146,11 @@ textarea {
   font: inherit;
 }
 
+textarea {
+  max-width: 100%;
+  min-width: 100%;
+}
+
 input:focus,
 textarea:focus {
   background-color: #f0e6fd;
@@ -125,12 +173,15 @@ h3 {
   font-size: 1rem;
 }
 
-.invalid label {
+.invalid div > label,
+.invalid p,
+p.invalid {
   color: red;
 }
 
 .invalid input,
 .invalid textarea {
   border: 1px solid red;
+  background: rgba(255, 0, 0, 0.24);
 }
 </style>
