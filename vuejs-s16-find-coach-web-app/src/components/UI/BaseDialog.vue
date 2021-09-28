2bc67e7b8c221b6f21f123b7cac9f700945de8ea
Artur Markowski <markowski.artur@outlook.com>
2021-09-29 01:15:54 +0200

    added transition to BaseDialog

--- vuejs-s16-find-coach-web-app/src/components/UI/BaseDialog.vue
+++ vuejs-s16-find-coach-web-app/src/components/UI/BaseDialog.vue
@@ -3,21 +3,23 @@
 <template>
   <teleport to="body">
     <div v-if="show" @click="tryClose" class="backdrop"></div>
-    <dialog open v-if="show">
-      <header>
-        <slot name="header">
-          <h2>{{ title }}</h2>
-        </slot>
-      </header>
-      <section>
-        <slot></slot>
-      </section>
-      <menu v-if="!fixed">
-        <slot name="actions">
-          <base-button @click="tryClose">Close</base-button>
-        </slot>
-      </menu>
-    </dialog>
+    <transition name="dialog">
+      <dialog open v-if="show">
+        <header>
+          <slot name="header">
+            <h2>{{ title }}</h2>
+          </slot>
+        </header>
+        <section>
+          <slot></slot>
+        </section>
+        <menu v-if="!fixed">
+          <slot name="actions">
+            <base-button @click="tryClose">Close</base-button>
+          </slot>
+        </menu>
+      </dialog>
+    </transition>
   </teleport>
 </template>
 
@@ -103,5 +105,22 @@ menu {
     left: calc(50% - 20rem);
     width: 40rem;
   }
+}
+
+.dialog-enter-from,
+.dialog-leave-to {
+  opacity: 0;
+  transform: scale(0.8);
+}
+.dialog-enter-to,
+.dialog-leave-from {
+  opacity: 1;
+  transform: scale(1);
+}
+.dialog-enter-active {
+  transition: all 0.3s ease-out;
+}
+.dialog-leave-active {
+  transition: all 0.3s ease-in;
 }
 </style>
