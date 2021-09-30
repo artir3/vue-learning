f915f4c0fc01db710bfd6392e4c1245a32b1cc97
Artur Markowski <markowski.artur@outlook.com>
2021-09-30 14:14:20 +0200

    Added autoLogin ad autoLogout; extracted properties and deleted fields

--- vuejs-s16-find-coach-web-app/src/store/modules/auth/actions.js
+++ vuejs-s16-find-coach-web-app/src/store/modules/auth/actions.js
@@ -1,23 +1,25 @@
+import { signupUrl, loginUrl } from '../../../../.env';
+
+let timer;
+
 export default {
   async login(context, payload) {
-    await callFirebase(
-      context,
-      context.state.loginUrl,
-      payload.email,
-      payload.password
-    );
+    await callFirebase(context, loginUrl, payload.email, payload.password);
   },
   async signup(context, payload) {
-    await callFirebase(
-      context,
-      context.state.signupUrl,
-      payload.email,
-      payload.password
-    );
+    await callFirebase(context, signupUrl, payload.email, payload.password);
   },
   autoLogin(context) {
     const token = localStorage.getItem('token');
     const userId = localStorage.getItem('userId');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+
+    const expiresIn = +tokenExpiration - new Date().getTime();
+    if (expiresIn < 10000) {
+      return;
+    }
+    setTimer(context, expiresIn);
+
     if (token && userId) {
       context.commit('setUser', {
         token,
@@ -26,11 +28,19 @@ export default {
       });
     }
   },
+  autoLogout(context) {
+    context.dispatch('logout');
+    context.commit('setAutoLogout');
+  },
   logout(context) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('tokenExpiration');
+
+    clearTimeout(timer);
     context.commit('setUser', {
       token: null,
-      userId: null,
-      tokenExpiration: null
+      userId: null
     });
   }
 };
@@ -49,15 +59,28 @@ async function callFirebase(context, url, email, password) {
   if (response.ok) {
     const data = {
       token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn
+      userId: responseData.localId
     };
+
+    const expiresIn = +responseData.expiresIn * 1000;
+    const expirationDate = new Date().getTime() + expiresIn;
+
     localStorage.setItem('token', data.token);
     localStorage.setItem('userId', data.userId);
+    localStorage.setItem('tokenExpiration', expirationDate);
+
+    setTimer(context, expiresIn);
+
     context.commit('setUser', data);
   } else {
     throw new Error(
       responseData.message || 'Failed to authenticate. Check your login data.'
     );
   }
+}
+
+function setTimer(context, expiresIn) {
+  timer = setTimeout(function() {
+    context.dispatch('autoLogout');
+  }, expiresIn);
 }
--- vuejs-s16-find-coach-web-app/src/store/modules/auth/index.js
+++ vuejs-s16-find-coach-web-app/src/store/modules/auth/index.js
@@ -5,13 +5,9 @@ import actions from './actions';
 export default {
   state() {
     return {
-      signupUrl:
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=***REMOVED***',
-      loginUrl:
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=***REMOVED***',
       userId: null,
       token: null,
-      tokenExpiration: null
+      didAutoLogout: false
     };
   },
   getters,
