b2aae818c53e4c4bdc6d5ba56275e838eaa3884f
Artur Markowski <markowski.artur@outlook.com>
2021-10-04 14:11:25 +0200

    Added computed values; manual receive data from inputs

---
+++ vuejs-s19-composition-api/src/03.ComputedValues.vue
@@ -0,0 +1,89 @@
+<template>
+  <section class="container">
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
+    <h3>{{ says }}</h3>
+    <button @click="setNewData">Update data</button>
+    <div>
+      <input
+        type="text"
+        name="firstName"
+        id="firstName"
+        placeholder="First Name"
+        @input="setFirstName"
+      />
+      <input
+        type="text"
+        name="lastName"
+        id="lastName"
+        placeholder="LastName"
+        @input="setLastName"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+import { reactive, ref, computed } from 'vue';
+export default {
+  setup() {
+    //its called once before "crating" a component
+    const user = reactive({
+      name: 'Lucifer',
+      age: 666
+    });
+    const says = ref('Good morning');
+    const firstName = ref('');
+    const lastName = ref('');
+
+    user.name = computed(() => {
+      return firstName.value + ' ' + lastName.value;
+    });
+
+    function setFirstName(event) {
+      firstName.value = event.target.value;
+    }
+
+    function setLastName(event) {
+      lastName.value = event.target.value;
+    }
+
+    const setNewData = () => {
+      user.name = 'Diabolo';
+      user.age = 32;
+      says.value = 'And Die';
+    };
+
+    return {
+      user,
+      says,
+      setNewData,
+      setFirstName,
+      setLastName
+    };
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
