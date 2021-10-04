2819dfaa16ee3e73ef4aafa28e7d8da055da1966
Artur Markowski <markowski.artur@outlook.com>
2021-10-04 16:58:32 +0200

    lifecycle hooks

--- vuejs-s19-composition-api/src/components/UserData.vue
+++ vuejs-s19-composition-api/src/components/UserData.vue
@@ -5,7 +5,16 @@
 </template>
 
 <script>
-import { computed, inject } from 'vue';
+import {
+  computed,
+  inject,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted
+} from 'vue';
 export default {
   props: ['firstName', 'lastName', 'says'],
   setup(props, context) {
@@ -14,6 +23,24 @@ export default {
     });
     const age = inject('user-age');
     context.emit('dummy-emit', 1);
+    onBeforeMount(() => {
+      console.log('on before mounted');
+    });
+    onMounted(() => {
+      console.log('on mounted');
+    });
+    onBeforeUpdate(() => {
+      console.log('on before update');
+    });
+    onUpdated(() => {
+      console.log('on updated');
+    });
+    onBeforeUnmount(() => {
+      console.log('on before unmounted');
+    });
+    onUnmounted(() => {
+      console.log('on unmounted');
+    });
     return { fullName, age };
   }
   //   computed: {
