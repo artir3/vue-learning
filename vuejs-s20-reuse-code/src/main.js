23842375fcf6b37e86e20853b6164efbb3b5c22d
Artur Markowski <markowski.artur@outlook.com>
2021-10-04 19:49:56 +0200

    cleanup

--- vuejs-s20-reuse-code/src/main.js
+++ vuejs-s20-reuse-code/src/main.js
@@ -1,11 +1,25 @@
 import { createApp } from 'vue';
 
+// 01.mixins
 // import App from './01.mixins/App.vue';
 // import loggerMixing from './01.mixins/mixins/logger.js';
 // const app = createApp(App);
 // app.mixin(loggerMixing);
 // app.mount('#app');
 
-import App from './02.reuse.compositionApi/App.vue';
+// 02.reuse.compositionApi
+// import App from './02.reuse.compositionApi/App.vue';
+// const app = createApp(App);
+// app.mount('#app');
+
+// 03.project
+import BaseSearch from './03.project/components/UI/BaseSearch.vue';
+import BaseContainer from './03.project/components/UI/BaseContainer.vue';
+import App from './03.project/App.vue';
+
 const app = createApp(App);
+
+app.component('base-search', BaseSearch);
+app.component('base-container', BaseContainer);
+
 app.mount('#app');
