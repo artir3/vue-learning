7f4f78ab51db007803cfe059e5535445fc0f608c
Artur Markowski <markowski.artur@outlook.com>
2021-10-04 19:47:08 +0200

    added composition reuse code; provide params into hooks

--- vuejs-s20-reuse-code/src/02.reuse.compositionApi/components/AddUser.vue
+++ vuejs-s20-reuse-code/src/02.reuse.compositionApi/components/AddUser.vue
@@ -1,5 +1,5 @@
 <template>
-  <user-alert v-if="alertIsVisible" title="Add a User?" @close="hideAlert">
+  <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
     <p>Do you want to continue with adding a user?</p>
   </user-alert>
   <section>
@@ -11,26 +11,22 @@
 <script>
 import { ref } from 'vue';
 import UserAlert from './UserAlert.vue';
+import useAlert from '../hooks/alert.js';
 
 export default {
   components: {
-    UserAlert,
+    UserAlert
   },
   setup() {
-    const alertIsVisible = ref(false);
+    const alertTitle = ref('Delete this user?!');
+    const alert = useAlert();
 
-    function showAlert() {
-      alertIsVisible.value = true;
-    }
-    function hideAlert() {
-      alertIsVisible.value = false;
-    }
-    
     return {
-      alertIsVisible,
-      showAlert,
-      hideAlert
+      alertIsVisible: alert[0],
+      showAlert: alert[1],
+      hideAlert: alert[2],
+      alertTitle
     };
-  },
+  }
 };
 </script>
