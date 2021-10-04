6f77218668594205f69e4ad22f138a3f63ad8b7b
Artur Markowski <markowski.artur@outlook.com>
2021-10-04 19:30:34 +0200

    add global mixin

--- vuejs-s20-reuse-code/src/main.js
+++ vuejs-s20-reuse-code/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 
 import App from './01.mixins/App.vue';
+import loggerMixing from './01.mixins/mixins/logger.js';
 
 const app = createApp(App);
+app.mixin(loggerMixing);
 
 app.mount('#app');
