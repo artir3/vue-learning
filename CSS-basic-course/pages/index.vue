eeb1ccf492306e7852473355a74e37f4874a814e
Artur Markowski <markowski.artur@outlook.com>
2021-10-20 22:14:35 +0200

    Gradients, multiple backgrounds

--- CSS-basic-course/pages/index.vue
+++ CSS-basic-course/pages/index.vue
@@ -19,18 +19,27 @@ export default {
 
 <style>
 #product-overview {
-  background: url("~~/assets/images/main-page.jpg") left 10% bottom 20% / cover
-    no-repeat border-box;
+  background: linear-gradient(to top, rgba(80, 68, 18, 0.6) 10%, transparent),
+    url("~~/assets/images/main-page.jpg") left 10% bottom 20% / cover no-repeat
+      border-box,
+    #f18484;
   /* background-image: url("~~/assets/images/main-page.jpg");
   background-position: left 10% bottom 20%;
-  background-size: cover; 
+  background-size: cover;
   background-repeat: no-repeat;
   background-origin: border-box;
   background-clip: border-box;*/
+  /* background-image: linear-gradient(30deg, #f18484 40%, #ff0606, #f18484); */
+  /* background-image: radial-gradient(
+    circle 300px at 15% 60%,
+    #f35 20%,
+    #43e 100%
+  ); */
   width: 100%;
   height: 600px;
   padding: 10px;
   margin-top: 43px;
+  /* border: 5px dashed red; */
   position: relative;
 }
 
